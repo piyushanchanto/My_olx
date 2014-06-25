bac871109d773c6de3b2e1d7dc6a33dcb558954f
piyushanchanto <piyush.awasthi@anchanto.com>
2014-06-25 12:59:46 +0530

    Added Spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyOlx::Application.config.secret_token = '574406583ce3d62d891dd1fd3d094c7b533fd9bf541b1ce77f55ef03cefee1479d22290eb47f67292607f6f09137faa20acec7a8e8daff00f9358c84cff45ccb'
